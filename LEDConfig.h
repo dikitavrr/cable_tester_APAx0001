4e42fd8059852079a2476279fa829a57ce84d10b
Mustafina Dinara <142242722+dikitavrr@users.noreply.github.com>
2023-08-15 11:23:36 +0300

    LEDConfig.h
    все define константы и условные обозначения

---
+++ LEDConfig.h
@@ -0,0 +1,65 @@
+/*
+ * LEDConfig.h
+ *
+ *  Created on: Aug 4, 2023
+ *      Author: User
+ */
+
+#ifndef LEDCONFIG_H_
+#define LEDCONFIG_H_
+
+#define CALL_RED_LED_5_POS 0
+#define CALL_RED_LED_6_POS 1
+#define CALL_RED_LED_7_POS 2
+#define CALL_RED_LED_8_POS 3
+#define CALL_RED_LED_1_POS 4
+#define CALL_RED_LED_2_POS 5
+#define CALL_RED_LED_3_POS 6
+#define CALL_RED_LED_4_POS 7
+
+#define CALL_GREEN_LED_4_POS 0
+#define CALL_GREEN_LED_3_POS 1
+#define CALL_GREEN_LED_2_POS 2
+#define CALL_GREEN_LED_1_POS 3
+#define CALL_GREEN_LED_8_POS 4
+#define CALL_GREEN_LED_7_POS 5
+#define CALL_GREEN_LED_6_POS 6
+#define CALL_GREEN_LED_5_POS 7
+
+#define RESP_GREEN_LED_4_POS 0
+#define RESP_GREEN_LED_3_POS 1
+#define RESP_GREEN_LED_2_POS 2
+#define RESP_GREEN_LED_1_POS 3
+#define RESP_GREEN_LED_8_POS 4
+#define RESP_GREEN_LED_7_POS 5
+#define RESP_GREEN_LED_6_POS 6
+#define RESP_GREEN_LED_5_POS 7
+
+#define RESP_RED_LED_5_POS 0
+#define RESP_RED_LED_6_POS 1
+#define RESP_RED_LED_7_POS 2
+#define RESP_RED_LED_8_POS 3
+#define RESP_RED_LED_1_POS 4
+#define RESP_RED_LED_2_POS 5
+#define RESP_RED_LED_3_POS 6
+#define RESP_RED_LED_4_POS 7
+
+#define LED_ON 1
+#define LED_OFF 0
+
+#define GREEN_COLOR 1
+#define RED_COLOR 2
+#define YELLOW_COLOR 0
+#define NUMBER_OF_COLORS 3
+#define USE_TIMER 1
+
+
+#define NUMBER_OF_LINES 8
+
+#define NUMBER_OF_SR 2
+
+#define SR_DATA_bm 0b00000001
+
+#define QUANTITY_OF_COLORS 3
+
+#endif /* LEDCONFIG_H_ */
