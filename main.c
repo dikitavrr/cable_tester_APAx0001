ea10f39923d7866c6a7688ab45711016d5ea48fa
Mustafina Dinara <142242722+dikitavrr@users.noreply.github.com>
2023-08-15 11:22:32 +0300

    main.c
    Рабочая подсветка и загрузка данных (з-к-ж с 1 по 8 линию и попеременное после цикла мигания з-к-ж все линии)

---
+++ main.c
@@ -0,0 +1,510 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2023 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include "LEDConfig.h"
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+TIM_HandleTypeDef htim3;
+
+/* USER CODE BEGIN PV */
+uint8_t LEDCallGreenData = 0;
+uint8_t LEDRespGreenData = 0;
+uint8_t LEDCallRedData = 0;
+uint8_t LEDRespRedData = 0;
+
+uint8_t AllLEDCallGreenData = 0;
+uint8_t AllLEDRespGreenData = 0;
+uint8_t AllLEDCallRedData = 0;
+uint8_t AllLEDRespRedData = 0;
+
+uint8_t ActiveRaw = 0;
+uint8_t ActiveRawColor = 1;
+uint8_t AllLinesUnicolor = 0;
+uint8_t ChangeColor = 1;
+uint8_t DisplayAllLinesUnicolor = 0;
+
+uint8_t NeedToDisplayLEDData = 1;
+uint8_t NeedToDefineLEDGreenData = 0;
+uint8_t NeedToDefineLEDRedData = 0;
+uint8_t NeedToDefineLEDNothingData = 0;
+
+uint8_t GreenCalls[NUMBER_OF_LINES] = {
+		CALL_GREEN_LED_8_POS,
+		CALL_GREEN_LED_7_POS,
+		CALL_GREEN_LED_6_POS,
+		CALL_GREEN_LED_5_POS,
+		CALL_GREEN_LED_4_POS,
+		CALL_GREEN_LED_3_POS,
+		CALL_GREEN_LED_2_POS,
+		CALL_GREEN_LED_1_POS};
+
+uint8_t GreenResponses[NUMBER_OF_LINES] = {
+		RESP_GREEN_LED_8_POS,
+		RESP_GREEN_LED_7_POS,
+		RESP_GREEN_LED_6_POS,
+		RESP_GREEN_LED_5_POS,
+		RESP_GREEN_LED_4_POS,
+		RESP_GREEN_LED_3_POS,
+		RESP_GREEN_LED_2_POS,
+		RESP_GREEN_LED_1_POS};
+
+uint8_t RedCalls[NUMBER_OF_LINES] = {
+		CALL_RED_LED_8_POS,
+		CALL_RED_LED_7_POS,
+		CALL_RED_LED_6_POS,
+		CALL_RED_LED_5_POS,
+		CALL_RED_LED_4_POS,
+		CALL_RED_LED_3_POS,
+		CALL_RED_LED_2_POS,
+		CALL_RED_LED_1_POS};
+
+uint8_t RedResponses[NUMBER_OF_LINES] = {
+		RESP_RED_LED_8_POS,
+		RESP_RED_LED_7_POS,
+		RESP_RED_LED_6_POS,
+		RESP_RED_LED_5_POS,
+		RESP_RED_LED_4_POS,
+		RESP_RED_LED_3_POS,
+		RESP_RED_LED_2_POS,
+		RESP_RED_LED_1_POS};
+
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_TIM3_Init(void);
+/* USER CODE BEGIN PFP */
+void ClearLEDSR(void);
+void LoadLEDSR(void);
+void ChangeColorLEDSR(void);
+void ChangeRawLEDSR(void);
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_TIM3_Init();
+  /* USER CODE BEGIN 2 */
+
+  // Установить все линии в первоначальное положение
+  HAL_GPIO_WritePin(STP_SR_LED_nCLR_GPIO_Port, STP_SR_LED_nCLR_Pin, GPIO_PIN_SET);
+  HAL_GPIO_WritePin(STP_SR_LED_CLK_GPIO_Port, STP_SR_LED_CLK_Pin, GPIO_PIN_RESET);
+
+  ClearLEDSR();
+
+  // Запустить таймер
+  if (USE_TIMER){
+
+  HAL_TIM_Base_Start_IT(&htim3);
+
+  }
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+
+//  NeedToDefineLEDGreenData = 1;
+
+  while (1)
+  {
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+
+	  //сформировали что отобразить
+
+	  if (ActiveRawColor == GREEN_COLOR) {
+			  LEDCallGreenData = LED_ON << GreenCalls[ActiveRaw];
+			  LEDRespGreenData = LED_ON << GreenResponses[ActiveRaw];
+			  LEDCallRedData = LED_OFF;
+			  LEDRespRedData = LED_OFF;
+	  }
+
+	  if (ActiveRawColor == RED_COLOR) {
+			  LEDCallRedData = LED_ON << RedCalls[ActiveRaw];
+			  LEDRespRedData = LED_ON << RedResponses[ActiveRaw];
+			  LEDCallGreenData = LED_OFF;
+			  LEDRespGreenData = LED_OFF;
+	  }
+	  if (ActiveRawColor == YELLOW_COLOR) {
+//			 LEDCallGreenData = LED_OFF;
+//			 LEDRespGreenData = LED_OFF;
+//			 LEDCallRedData = LED_OFF;
+//			 LEDRespRedData = LED_OFF;
+
+			 LEDCallGreenData = LED_ON << GreenCalls[ActiveRaw];
+			 LEDRespGreenData = LED_ON << GreenResponses[ActiveRaw];
+			 LEDCallRedData = LED_ON << RedCalls[ActiveRaw];
+			 LEDRespRedData = LED_ON << RedResponses[ActiveRaw];
+	  }
+
+	  //отобразили
+
+	  if (NeedToDisplayLEDData) {
+		  ClearLEDSR();
+		  LoadLEDSR();
+
+		  if (USE_TIMER) {
+			  NeedToDisplayLEDData = 0;
+		  }
+
+		  ActiveRawColor++;
+
+		  if (ActiveRawColor == NUMBER_OF_COLORS) {
+			  ActiveRawColor = 0;
+		  }
+
+		  if (ActiveRawColor == YELLOW_COLOR) {
+			  ActiveRaw++;
+
+			  if (ActiveRaw == NUMBER_OF_LINES) {
+				  ActiveRaw = 0;
+				  AllLinesUnicolor++;
+				  if (AllLinesUnicolor == NUMBER_OF_COLORS) {
+					  AllLinesUnicolor = 0;
+				  }
+				  DisplayAllLinesUnicolor = 1;
+			  }
+		  }
+	  }
+
+	  if (AllLinesUnicolor == GREEN_COLOR) {
+
+		  //ClearLEDSR();
+		  AllLEDCallGreenData = LED_ON;
+		  AllLEDRespGreenData = LED_ON;
+		  AllLEDCallRedData = LED_OFF;
+		  AllLEDRespRedData = LED_OFF;
+		  //LoadLEDSR();
+	  }
+	  if (AllLinesUnicolor == RED_COLOR) {
+
+		  //ClearLEDSR();
+		  AllLEDCallGreenData = LED_OFF;
+		  AllLEDRespGreenData = LED_OFF;
+		  AllLEDCallRedData = LED_ON;
+		  AllLEDRespRedData = LED_ON;
+		  //LoadLEDSR();
+	  }
+	  if (AllLinesUnicolor == YELLOW_COLOR) {
+
+		  //ClearLEDSR();
+		  AllLEDCallGreenData = LED_ON;            //так горит жёлтый, в зависимости от того, что OFF будет другой цвет - цикл?? условие7??? чтобы по порядку с прерыванием
+		  AllLEDRespGreenData = LED_ON;
+		  AllLEDCallRedData = LED_ON;
+		  AllLEDRespRedData = LED_ON;
+		  //LoadLEDSR();
+	  }
+
+
+	  if (DisplayAllLinesUnicolor){
+
+
+	      HAL_GPIO_WritePin(LED_CALL_GREEN_SR_DATA_GPIO_Port, LED_CALL_GREEN_SR_DATA_Pin, AllLEDCallGreenData);
+		  HAL_GPIO_WritePin(LED_RESP_GREEN_SR_DATA_GPIO_Port, LED_RESP_GREEN_SR_DATA_Pin, AllLEDRespGreenData);
+		  HAL_GPIO_WritePin(LED_CALL_RED_SR_DATA_GPIO_Port, LED_CALL_RED_SR_DATA_Pin, AllLEDCallRedData);
+		  HAL_GPIO_WritePin(LED_RESP_RED_SR_DATA_GPIO_Port, LED_RESP_RED_SR_DATA_Pin, AllLEDRespRedData);
+
+			for (uint8_t n = 0; n < NUMBER_OF_LINES; n++) {
+
+				HAL_GPIO_WritePin(STP_SR_LED_CLK_GPIO_Port, STP_SR_LED_CLK_Pin, GPIO_PIN_SET);
+				HAL_GPIO_WritePin(STP_SR_LED_CLK_GPIO_Port, STP_SR_LED_CLK_Pin, GPIO_PIN_RESET);
+			}
+			DisplayAllLinesUnicolor = 0;
+	  }
+
+	  if (!USE_TIMER) {
+
+		  ActiveRawColor++;
+			 if (ActiveRawColor == NUMBER_OF_COLORS) {
+				 ActiveRawColor = 0;
+			 }
+	  }
+
+//	  HAL_Delay(500);
+
+	  //запретили менять уже отображенное
+
+//	  NeedToDefineLEDGreenData = 0;
+
+	  //меняем данные
+
+
+	  //�?зменить данные для отображения
+
+
+	  //Хочу чтобы:
+	  // Сначала горит линия 1 зеленым
+	  // Затем горит линия 1 красным (потом)
+	  // Затем линия 1 гаснет
+	  // Процесс повторяется для лини 2, 3 ... 8
+
+
+	  // Загораются все зеленые
+	  // загораются все красные
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL4;
+  RCC_OscInitStruct.PLL.PREDIV = RCC_PREDIV_DIV1;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV2;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief TIM3 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM3_Init(void)
+{
+
+  /* USER CODE BEGIN TIM3_Init 0 */
+
+  /* USER CODE END TIM3_Init 0 */
+
+  TIM_ClockConfigTypeDef sClockSourceConfig = {0};
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
+
+  /* USER CODE BEGIN TIM3_Init 1 */
+
+  /* USER CODE END TIM3_Init 1 */
+  htim3.Instance = TIM3;
+  htim3.Init.Prescaler = 124;
+  htim3.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim3.Init.Period = 32499;
+  htim3.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim3.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  if (HAL_TIM_Base_Init(&htim3) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
+  if (HAL_TIM_ConfigClockSource(&htim3, &sClockSourceConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim3, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM3_Init 2 */
+
+  /* USER CODE END TIM3_Init 2 */
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOA, LED_RESP_RED_SR_DATA_Pin|LED_RESP_GREEN_SR_DATA_Pin|LED_CALL_RED_SR_DATA_Pin|LED_CALL_GREEN_SR_DATA_Pin
+                          |STP_SR_LED_nCLR_Pin|STP_SR_LED_CLK_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pins : LED_RESP_RED_SR_DATA_Pin LED_RESP_GREEN_SR_DATA_Pin LED_CALL_RED_SR_DATA_Pin LED_CALL_GREEN_SR_DATA_Pin
+                           STP_SR_LED_nCLR_Pin STP_SR_LED_CLK_Pin */
+  GPIO_InitStruct.Pin = LED_RESP_RED_SR_DATA_Pin|LED_RESP_GREEN_SR_DATA_Pin|LED_CALL_RED_SR_DATA_Pin|LED_CALL_GREEN_SR_DATA_Pin
+                          |STP_SR_LED_nCLR_Pin|STP_SR_LED_CLK_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_MEDIUM;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+}
+
+/* USER CODE BEGIN 4 */
+
+void ClearLEDSR(void) {
+
+	HAL_GPIO_WritePin(STP_SR_LED_nCLR_GPIO_Port, STP_SR_LED_nCLR_Pin, GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(STP_SR_LED_nCLR_GPIO_Port, STP_SR_LED_nCLR_Pin, GPIO_PIN_SET);
+}
+
+void LoadLEDSR(void) {
+
+	for (uint8_t i = 0; i < NUMBER_OF_LINES; i++) {
+        HAL_GPIO_WritePin(LED_CALL_GREEN_SR_DATA_GPIO_Port, LED_CALL_GREEN_SR_DATA_Pin, (LEDCallGreenData & SR_DATA_bm));
+		HAL_GPIO_WritePin(LED_RESP_GREEN_SR_DATA_GPIO_Port, LED_RESP_GREEN_SR_DATA_Pin, (LEDRespGreenData & SR_DATA_bm));
+		HAL_GPIO_WritePin(LED_CALL_RED_SR_DATA_GPIO_Port, LED_CALL_RED_SR_DATA_Pin, (LEDCallRedData & SR_DATA_bm));
+		HAL_GPIO_WritePin(LED_RESP_RED_SR_DATA_GPIO_Port, LED_RESP_RED_SR_DATA_Pin, (LEDRespRedData & SR_DATA_bm));
+
+		HAL_GPIO_WritePin(STP_SR_LED_CLK_GPIO_Port, STP_SR_LED_CLK_Pin, GPIO_PIN_SET);
+		HAL_GPIO_WritePin(STP_SR_LED_CLK_GPIO_Port, STP_SR_LED_CLK_Pin, GPIO_PIN_RESET);
+
+		LEDCallGreenData = LEDCallGreenData >> 1;
+		LEDRespGreenData = LEDRespGreenData >> 1;
+		LEDCallRedData = LEDCallRedData >> 1;
+		LEDRespRedData = LEDRespRedData >> 1;
+	}
+}
+
+//void ChangeRawLEDSR(void){
+//	ActiveRaw++;
+//		  if (ActiveRaw == NUMBER_OF_LINES) {
+//			  ActiveRaw = 0;
+//		  }
+//}
+//
+//void ChangeColorLEDSR(void){
+//	ActiveRawColor++;
+//	if (ActiveRawColor == NUMBER_OF_COLORS){
+//		ActiveRawColor = 0;
+//	}
+//}
+
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) {
+
+	if (htim == &htim3)
+	{
+		NeedToDisplayLEDData = 1;
+///**/		if (ActiveRaw == NUMBER_OF_LINES) {
+//			AllLinesUnicolor = 1;
+///**/		}
+	}
+}
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
